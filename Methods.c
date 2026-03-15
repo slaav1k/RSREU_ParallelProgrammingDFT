d0fc4b8f751cd3608e36d1bef4d6436ee3a3c837
slaav1k <slawa.121@yandex.ru>
2026-03-15 19:39:45 +0300

    fix mpi

--- Methods.c
+++ Methods.c
@@ -2,382 +2,302 @@
 #include <stdlib.h>
 #include <math.h>
 #include "Methods.h"
+
 #ifdef USE_MPI
 #include <mpi.h>
 #endif
-#ifdef _OPENMP
-#include <omp.h>
-#endif
-
-static progress_cb_t g_progress_cb = NULL;
-
-void set_progress_callback(progress_cb_t cb) {
-    g_progress_cb = cb;
-}
-
-static int g_use_omp = 0;
-void set_use_omp(int use) { g_use_omp = use; }
 
 static int g_use_mpi = 0;
+
 void set_use_mpi(int use) { g_use_mpi = use; }
 
-void set_num_threads(int n) {
-#if defined(_OPENMP)
-    if (n > 0) omp_set_num_threads(n);
-#endif
-}
+void dft(const cplx in[], cplx out[], int n)
+{
+    for (int k = 0; k < n; k++)
+    {
+        cplx sum = { 0,0 };
 
-// Naive forward DFT
-void dft(const cplx in[], cplx out[], int n) {
-    for (int k = 0; k < n; k++) {
-        cplx sum = { 0.0, 0.0 };
-        for (int m = 0; m < n; m++) {
+        for (int m = 0; m < n; m++)
+        {
             double angle = -2 * PI * k * m / (double)n;
             cplx w = cplx_from_polar(1.0, angle);
-            cplx prod = cplx_mul(in[m], w);
-            sum = cplx_add(sum, prod);
+
+            sum = cplx_add(sum, cplx_mul(in[m], w));
         }
+
         out[k] = sum;
     }
 }
 
-// Inverse DFT (almost the same, plus sign and divide by n)
-void idft(const cplx in[], cplx out[], int n) {
-    for (int k = 0; k < n; k++) {
-        cplx sum = { 0.0, 0.0 };
-        for (int m = 0; m < n; m++) {
-            double angle = +2 * PI * k * m / (double)n;
+void idft(const cplx in[], cplx out[], int n)
+{
+    for (int k = 0; k < n; k++)
+    {
+        cplx sum = { 0,0 };
+
+        for (int m = 0; m < n; m++)
+        {
+            double angle = 2 * PI * k * m / (double)n;
             cplx w = cplx_from_polar(1.0, angle);
-            cplx prod = cplx_mul(in[m], w);
-            sum = cplx_add(sum, prod);
+
+            sum = cplx_add(sum, cplx_mul(in[m], w));
         }
-        out[k] = cplx_scale(sum, 1.0 / (double)n);
+
+        out[k] = cplx_scale(sum, 1.0 / n);
     }
 }
 
-// 2D DFT: perform 1D DFT on rows, then on columns
-void dft2d(const cplx* in, cplx* out, int width, int height) {
-#if defined(USE_MPI)
-    if (g_use_mpi) {
-        int rank = 0, size = 1;
-        MPI_Comm_size(MPI_COMM_WORLD, &size);
+void dft2d(const cplx* in, cplx* out, int width, int height)
+{
+
+#ifdef USE_MPI
+    if (g_use_mpi)
+    {
+        int rank, size;
+
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-        // Распределение строк
-        int base = height / size;
-        int rem = height % size;
+        int rows_per_proc = height / size;
+        int remainder = height % size;
 
-        int* rows = (int*)malloc(sizeof(int) * size);
-        int* displs_rows = (int*)malloc(sizeof(int) * size);
+        int local_rows = rows_per_proc + (rank < remainder ? 1 : 0);
+
+        int* sendcounts = malloc(sizeof(int) * size);
+        int* displs = malloc(sizeof(int) * size);
 
         int offset = 0;
-        for (int i = 0; i < size; ++i) {
-            rows[i] = base + (i < rem ? 1 : 0);
-            displs_rows[i] = offset;
-            offset += rows[i];
+
+        for (int i = 0; i < size; i++)
+        {
+            sendcounts[i] = (rows_per_proc + (i < remainder ? 1 : 0)) * width;
+            displs[i] = offset;
+            offset += sendcounts[i];
         }
 
-        int local_rows = rows[rank];
+        cplx* local_in = malloc(sizeof(cplx) * local_rows * width);
 
-        // sendcounts в double (2 double на cplx)
-        int* sendcounts_dbl = (int*)malloc(sizeof(int) * size);
-        int* senddispls_dbl = (int*)malloc(sizeof(int) * size);
+        MPI_Scatterv(in,
+            sendcounts,
+            displs,
+            MPI_DOUBLE_COMPLEX,
+            local_in,
+            sendcounts[rank],
+            MPI_DOUBLE_COMPLEX,
+            0,
+            MPI_COMM_WORLD);
 
-        for (int i = 0; i < size; ++i) {
-            sendcounts_dbl[i] = rows[i] * width * 2;
-            senddispls_dbl[i] = displs_rows[i] * width * 2;
-        }
+        cplx* local_temp = malloc(sizeof(cplx) * local_rows * width);
 
-        // Подготовка данных на root
-        double* sendbuf_dbl = NULL;
-        if (rank == 0) {
-            sendbuf_dbl = (double*)malloc(height * width * 2 * sizeof(double));
-            for (int i = 0; i < height * width; i++) {
-                sendbuf_dbl[i * 2] = in[i].re;
-                sendbuf_dbl[i * 2 + 1] = in[i].im;
-            }
-        }
-
-        double* recvbuf_dbl = (double*)malloc(local_rows * width * 2 * sizeof(double));
-
-        MPI_Barrier(MPI_COMM_WORLD);
-
-        // Scatterv
-        MPI_Scatterv(sendbuf_dbl, sendcounts_dbl, senddispls_dbl, MPI_DOUBLE,
-            recvbuf_dbl, sendcounts_dbl[rank], MPI_DOUBLE,
-            0, MPI_COMM_WORLD);
-
-        // Конвертация обратно в cplx
-        cplx* local_in = (cplx*)malloc(sizeof(cplx) * local_rows * width);
-        for (int i = 0; i < local_rows * width; i++) {
-            local_in[i].re = recvbuf_dbl[i * 2];
-            local_in[i].im = recvbuf_dbl[i * 2 + 1];
-        }
-
-        // Локальное DFT по строкам
-        cplx* local_temp = (cplx*)malloc(sizeof(cplx) * local_rows * width);
-        for (int r = 0; r < local_rows; ++r) {
+        for (int r = 0; r < local_rows; r++)
             dft(&local_in[r * width], &local_temp[r * width], width);
-        }
 
-        // Конвертируем в double для передачи
-        double* local_temp_dbl = (double*)malloc(sizeof(double) * local_rows * width * 2);
-        for (int i = 0; i < local_rows * width; i++) {
-            local_temp_dbl[i * 2] = local_temp[i].re;
-            local_temp_dbl[i * 2 + 1] = local_temp[i].im;
-        }
+        cplx* temp_full = malloc(sizeof(cplx) * width * height);
 
-        // Собираем все результаты
-        double* temp_all_dbl = (double*)malloc(sizeof(double) * width * height * 2);
-        MPI_Allgatherv(local_temp_dbl, sendcounts_dbl[rank], MPI_DOUBLE,
-            temp_all_dbl, sendcounts_dbl, senddispls_dbl, MPI_DOUBLE, MPI_COMM_WORLD);
+        MPI_Gatherv(local_temp,
+            sendcounts[rank],
+            MPI_DOUBLE_COMPLEX,
+            temp_full,
+            sendcounts,
+            displs,
+            MPI_DOUBLE_COMPLEX,
+            0,
+            MPI_COMM_WORLD);
 
-        // Конвертируем обратно в cplx
-        cplx* temp_all = (cplx*)malloc(sizeof(cplx) * width * height);
-        for (int i = 0; i < width * height; i++) {
-            temp_all[i].re = temp_all_dbl[i * 2];
-            temp_all[i].im = temp_all_dbl[i * 2 + 1];
-        }
+        MPI_Bcast(temp_full, width * height, MPI_DOUBLE_COMPLEX, 0, MPI_COMM_WORLD);
 
-        // DFT по столбцам
-        cplx* out_full = (cplx*)malloc(sizeof(cplx) * width * height);
-        for (int x = 0; x < width; ++x) {
-            cplx* col_in = (cplx*)malloc(sizeof(cplx) * height);
-            cplx* col_out = (cplx*)malloc(sizeof(cplx) * height);
-            for (int y = 0; y < height; ++y) {
-                col_in[y] = temp_all[y * width + x];
-            }
+        int cols_per_proc = width / size;
+        int col_rem = width % size;
+
+        int start = rank * cols_per_proc + (rank < col_rem ? rank : col_rem);
+        int count = cols_per_proc + (rank < col_rem ? 1 : 0);
+
+        cplx* col_in = malloc(sizeof(cplx) * height);
+        cplx* col_out = malloc(sizeof(cplx) * height);
+
+        cplx* partial = calloc(width * height, sizeof(cplx));
+
+        for (int x = start; x < start + count; x++)
+        {
+            for (int y = 0; y < height; y++)
+                col_in[y] = temp_full[y * width + x];
+
             dft(col_in, col_out, height);
-            for (int y = 0; y < height; ++y) {
-                out_full[y * width + x] = col_out[y];
-            }
-            free(col_in);
-            free(col_out);
+
+            for (int y = 0; y < height; y++)
+                partial[y * width + x] = col_out[y];
         }
 
-        // Конвертируем результат в double
-        double* out_full_dbl = (double*)malloc(sizeof(double) * width * height * 2);
-        for (int i = 0; i < width * height; i++) {
-            out_full_dbl[i * 2] = out_full[i].re;
-            out_full_dbl[i * 2 + 1] = out_full[i].im;
-        }
+        MPI_Reduce(partial,
+            out,
+            width * height,
+            MPI_DOUBLE_COMPLEX,
+            MPI_SUM,
+            0,
+            MPI_COMM_WORLD);
 
-        // Собираем на root
-        double* out_dbl = (rank == 0) ? (double*)malloc(sizeof(double) * width * height * 2) : NULL;
-        MPI_Gatherv(out_full_dbl + displs_rows[rank] * width * 2, sendcounts_dbl[rank], MPI_DOUBLE,
-            out_dbl, sendcounts_dbl, senddispls_dbl, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-        // Конвертируем финальный результат
-        if (rank == 0) {
-            for (int i = 0; i < width * height; i++) {
-                out[i].re = out_dbl[i * 2];
-                out[i].im = out_dbl[i * 2 + 1];
-            }
-            free(out_dbl);
-        }
-
-        // Очистка
-        free(rows);
-        free(displs_rows);
-        free(sendcounts_dbl);
-        free(senddispls_dbl);
-        free(sendbuf_dbl);
-        free(recvbuf_dbl);
         free(local_in);
         free(local_temp);
-        free(local_temp_dbl);
-        free(temp_all_dbl);
-        free(temp_all);
-        free(out_full);
-        free(out_full_dbl);
+        free(temp_full);
+        free(col_in);
+        free(col_out);
+        free(partial);
+        free(sendcounts);
+        free(displs);
 
         return;
     }
 #endif
 
-    // Не-MPI версия
-    cplx* temp = (cplx*)malloc(sizeof(cplx) * width * height);
-    if (!temp) return;
+    cplx* temp = malloc(sizeof(cplx) * width * height);
 
-    // rows
-    for (int y = 0; y < height; ++y) {
+    for (int y = 0; y < height; y++)
         dft(&in[y * width], &temp[y * width], width);
-        if (g_progress_cb) g_progress_cb("Rows (forward)", y + 1, height);
+
+    cplx* col_in = malloc(sizeof(cplx) * height);
+    cplx* col_out = malloc(sizeof(cplx) * height);
+
+    for (int x = 0; x < width; x++)
+    {
+        for (int y = 0; y < height; y++)
+            col_in[y] = temp[y * width + x];
+
+        dft(col_in, col_out, height);
+
+        for (int y = 0; y < height; y++)
+            out[y * width + x] = col_out[y];
     }
 
-    // columns
-    for (int x = 0; x < width; ++x) {
-        cplx* col_in = (cplx*)malloc(sizeof(cplx) * height);
-        cplx* col_out = (cplx*)malloc(sizeof(cplx) * height);
-        for (int y = 0; y < height; ++y) col_in[y] = temp[y * width + x];
-        dft(col_in, col_out, height);
-        for (int y = 0; y < height; ++y) out[y * width + x] = col_out[y];
-        free(col_in);
-        free(col_out);
-        if (g_progress_cb) g_progress_cb("Cols (forward)", x + 1, width);
-    }
+    free(col_in);
+    free(col_out);
     free(temp);
 }
 
-void idft2d(const cplx* in, cplx* out, int width, int height) {
-#if defined(USE_MPI)
-    if (g_use_mpi) {
-        int rank = 0, size = 1;
-        MPI_Comm_size(MPI_COMM_WORLD, &size);
+void idft2d(const cplx* in, cplx* out, int width, int height)
+{
+
+#ifdef USE_MPI
+    if (g_use_mpi)
+    {
+        int rank, size;
+
         MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+        MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-        // Распределение строк
-        int base = height / size;
-        int rem = height % size;
+        int rows_per_proc = height / size;
+        int remainder = height % size;
 
-        int* rows = (int*)malloc(sizeof(int) * size);
-        int* displs_rows = (int*)malloc(sizeof(int) * size);
+        int local_rows = rows_per_proc + (rank < remainder ? 1 : 0);
+
+        int* sendcounts = malloc(sizeof(int) * size);
+        int* displs = malloc(sizeof(int) * size);
 
         int offset = 0;
-        for (int i = 0; i < size; ++i) {
-            rows[i] = base + (i < rem ? 1 : 0);
-            displs_rows[i] = offset;
-            offset += rows[i];
+
+        for (int i = 0; i < size; i++)
+        {
+            sendcounts[i] = (rows_per_proc + (i < remainder ? 1 : 0)) * width;
+            displs[i] = offset;
+            offset += sendcounts[i];
         }
 
-        int local_rows = rows[rank];
+        cplx* local_in = malloc(sizeof(cplx) * local_rows * width);
 
-        // sendcounts в double (2 double на cplx)
-        int* sendcounts_dbl = (int*)malloc(sizeof(int) * size);
-        int* senddispls_dbl = (int*)malloc(sizeof(int) * size);
+        MPI_Scatterv(in,
+            sendcounts,
+            displs,
+            MPI_DOUBLE_COMPLEX,
+            local_in,
+            sendcounts[rank],
+            MPI_DOUBLE_COMPLEX,
+            0,
+            MPI_COMM_WORLD);
 
-        for (int i = 0; i < size; ++i) {
-            sendcounts_dbl[i] = rows[i] * width * 2;
-            senddispls_dbl[i] = displs_rows[i] * width * 2;
-        }
+        cplx* local_temp = malloc(sizeof(cplx) * local_rows * width);
 
-        // Подготовка данных на root
-        double* sendbuf_dbl = NULL;
-        if (rank == 0) {
-            sendbuf_dbl = (double*)malloc(height * width * 2 * sizeof(double));
-            for (int i = 0; i < height * width; i++) {
-                sendbuf_dbl[i * 2] = in[i].re;
-                sendbuf_dbl[i * 2 + 1] = in[i].im;
-            }
-        }
-
-        double* recvbuf_dbl = (double*)malloc(local_rows * width * 2 * sizeof(double));
-
-        MPI_Barrier(MPI_COMM_WORLD);
-
-        // Scatterv
-        MPI_Scatterv(sendbuf_dbl, sendcounts_dbl, senddispls_dbl, MPI_DOUBLE,
-            recvbuf_dbl, sendcounts_dbl[rank], MPI_DOUBLE,
-            0, MPI_COMM_WORLD);
-
-        // Конвертация обратно в cplx
-        cplx* local_in = (cplx*)malloc(sizeof(cplx) * local_rows * width);
-        for (int i = 0; i < local_rows * width; i++) {
-            local_in[i].re = recvbuf_dbl[i * 2];
-            local_in[i].im = recvbuf_dbl[i * 2 + 1];
-        }
-
-        // Локальное IDFT по строкам
-        cplx* local_temp = (cplx*)malloc(sizeof(cplx) * local_rows * width);
-        for (int r = 0; r < local_rows; ++r) {
+        // IDFT по строкам
+        for (int r = 0; r < local_rows; r++)
             idft(&local_in[r * width], &local_temp[r * width], width);
-        }
 
-        // Конвертируем в double для передачи
-        double* local_temp_dbl = (double*)malloc(sizeof(double) * local_rows * width * 2);
-        for (int i = 0; i < local_rows * width; i++) {
-            local_temp_dbl[i * 2] = local_temp[i].re;
-            local_temp_dbl[i * 2 + 1] = local_temp[i].im;
-        }
+        cplx* temp_full = malloc(sizeof(cplx) * width * height);
 
-        // Собираем все результаты
-        double* temp_all_dbl = (double*)malloc(sizeof(double) * width * height * 2);
-        MPI_Allgatherv(local_temp_dbl, sendcounts_dbl[rank], MPI_DOUBLE,
-            temp_all_dbl, sendcounts_dbl, senddispls_dbl, MPI_DOUBLE, MPI_COMM_WORLD);
+        MPI_Gatherv(local_temp,
+            sendcounts[rank],
+            MPI_DOUBLE_COMPLEX,
+            temp_full,
+            sendcounts,
+            displs,
+            MPI_DOUBLE_COMPLEX,
+            0,
+            MPI_COMM_WORLD);
 
-        // Конвертируем обратно в cplx
-        cplx* temp_all = (cplx*)malloc(sizeof(cplx) * width * height);
-        for (int i = 0; i < width * height; i++) {
-            temp_all[i].re = temp_all_dbl[i * 2];
-            temp_all[i].im = temp_all_dbl[i * 2 + 1];
-        }
+        MPI_Bcast(temp_full, width * height, MPI_DOUBLE_COMPLEX, 0, MPI_COMM_WORLD);
+
+        int cols_per_proc = width / size;
+        int col_rem = width % size;
+
+        int start = rank * cols_per_proc + (rank < col_rem ? rank : col_rem);
+        int count = cols_per_proc + (rank < col_rem ? 1 : 0);
+
+        cplx* col_in = malloc(sizeof(cplx) * height);
+        cplx* col_out = malloc(sizeof(cplx) * height);
+
+        cplx* partial = calloc(width * height, sizeof(cplx));
 
         // IDFT по столбцам
-        cplx* out_full = (cplx*)malloc(sizeof(cplx) * width * height);
-        for (int x = 0; x < width; ++x) {
-            cplx* col_in = (cplx*)malloc(sizeof(cplx) * height);
-            cplx* col_out = (cplx*)malloc(sizeof(cplx) * height);
-            for (int y = 0; y < height; ++y) {
-                col_in[y] = temp_all[y * width + x];
-            }
+        for (int x = start; x < start + count; x++)
+        {
+            for (int y = 0; y < height; y++)
+                col_in[y] = temp_full[y * width + x];
+
             idft(col_in, col_out, height);
-            for (int y = 0; y < height; ++y) {
-                out_full[y * width + x] = col_out[y];
-            }
-            free(col_in);
-            free(col_out);
+
+            for (int y = 0; y < height; y++)
+                partial[y * width + x] = col_out[y];
         }
 
-        // Конвертируем результат в double
-        double* out_full_dbl = (double*)malloc(sizeof(double) * width * height * 2);
-        for (int i = 0; i < width * height; i++) {
-            out_full_dbl[i * 2] = out_full[i].re;
-            out_full_dbl[i * 2 + 1] = out_full[i].im;
-        }
+        MPI_Reduce(partial,
+            out,
+            width * height,
+            MPI_DOUBLE_COMPLEX,
+            MPI_SUM,
+            0,
+            MPI_COMM_WORLD);
 
-        // Собираем на root
-        double* out_dbl = (rank == 0) ? (double*)malloc(sizeof(double) * width * height * 2) : NULL;
-        MPI_Gatherv(out_full_dbl + displs_rows[rank] * width * 2, sendcounts_dbl[rank], MPI_DOUBLE,
-            out_dbl, sendcounts_dbl, senddispls_dbl, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-        // Конвертируем финальный результат
-        if (rank == 0) {
-            for (int i = 0; i < width * height; i++) {
-                out[i].re = out_dbl[i * 2];
-                out[i].im = out_dbl[i * 2 + 1];
-            }
-            free(out_dbl);
-        }
-
-        // Очистка
-        free(rows);
-        free(displs_rows);
-        free(sendcounts_dbl);
-        free(senddispls_dbl);
-        free(sendbuf_dbl);
-        free(recvbuf_dbl);
         free(local_in);
         free(local_temp);
-        free(local_temp_dbl);
-        free(temp_all_dbl);
-        free(temp_all);
-        free(out_full);
-        free(out_full_dbl);
+        free(temp_full);
+        free(col_in);
+        free(col_out);
+        free(partial);
+        free(sendcounts);
+        free(displs);
 
         return;
     }
 #endif
 
-    // Не-MPI версия
-    cplx* temp = (cplx*)malloc(sizeof(cplx) * width * height);
-    if (!temp) return;
+    // обычная версия без MPI
+    cplx* temp = malloc(sizeof(cplx) * width * height);
 
-    // columns (inverse)
-    for (int x = 0; x < width; ++x) {
-        cplx* col_in = (cplx*)malloc(sizeof(cplx) * height);
-        cplx* col_out = (cplx*)malloc(sizeof(cplx) * height);
-        for (int y = 0; y < height; ++y) col_in[y] = in[y * width + x];
+    cplx* col_in = malloc(sizeof(cplx) * height);
+    cplx* col_out = malloc(sizeof(cplx) * height);
+
+    for (int x = 0; x < width; x++)
+    {
+        for (int y = 0; y < height; y++)
+            col_in[y] = in[y * width + x];
+
         idft(col_in, col_out, height);
-        for (int y = 0; y < height; ++y) temp[y * width + x] = col_out[y];
-        free(col_in);
-        free(col_out);
-        if (g_progress_cb) g_progress_cb("Cols (inverse)", x + 1, width);
+
+        for (int y = 0; y < height; y++)
+            temp[y * width + x] = col_out[y];
     }
 
-    // rows (inverse)
-    for (int y = 0; y < height; ++y) {
+    for (int y = 0; y < height; y++)
         idft(&temp[y * width], &out[y * width], width);
-        if (g_progress_cb) g_progress_cb("Rows (inverse)", y + 1, height);
-    }
+
+    free(col_in);
+    free(col_out);
     free(temp);
 }
--- ParallelProgramming.c
+++ ParallelProgramming.c
@@ -2,182 +2,98 @@
 #include "Methods.h"
 #include "Image.h"
 #include "Transform.h"
-#include <string.h>
 #include <stdlib.h>
-#include <time.h>
+
 #ifdef USE_MPI
 #include <mpi.h>
 #endif
 
-// Прогресс бар
-static void progress_cb(const char* stage, int current, int total) {
-    int pct = (int)((current * 100) / (double)total);
-    int bar_width = 30;
-    int filled = (pct * bar_width) / 100;
-    printf("\r%16s: %4d/%4d [", stage, current, total);
-    for (int i = 0; i < filled; ++i) putchar('=');
-    for (int i = filled; i < bar_width; ++i) putchar(' ');
-    printf("] %3d%%", pct);
-    if (current >= total) putchar('\n');
-    fflush(stdout);
-}
+int main(int argc, char** argv)
+{
 
-// Масштабирование изображения (ближайший сосед) и сохранение
-static int scale_and_save_from_pixels(unsigned char* pixels, int width, int height, const char* out_path, int scale) {
-    if (!pixels || width <= 0 || height <= 0 || scale <= 0) return -1;
-    int w2 = width * scale;
-    int h2 = height * scale;
-    unsigned char* out = (unsigned char*)malloc((size_t)w2 * h2);
-    if (!out) return -1;
-    for (int y = 0; y < h2; ++y) {
-        int sy = y / scale;
-        for (int x = 0; x < w2; ++x) {
-            int sx = x / scale;
-            out[y * w2 + x] = pixels[sy * width + sx];
-        }
-    }
-    int res = save_bmp_grayscale(out_path, out, w2, h2);
-    if (res == 0) printf("Scaled image saved: %s\n", out_path);
-    else printf("Failed to save scaled image: %s\n", out_path);
-    free(out);
-    return res;
-}
+    int rank = 0;
+    int size = 1;
 
-int main(int argc, char** argv) {
-    // Включить/выключить OMP / MPI здесь в коде
-    int flagOMP = 0;
-    int flagMPI = 1; // set to 1 to enable MPI mode (requires build with USE_MPI and mpiexec)
-    int use_omp = flagOMP;
-    int use_mpi = flagMPI;
-    const char* in_file = "resources/input.bmp";
-    const char* spectrum_file = "resources/spectrum.bmp";
-    const char* recon_file = "resources/recovered.bmp";
+#ifdef USE_MPI
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    set_use_mpi(1);
 
-    int mpi_rank = 0, mpi_size = 1;
-    unsigned char* pixels = NULL;
-    int width = 0, height = 0;
-
-    if (use_omp) {
-        set_use_omp(1);
-        int omp_threads = 12;
-        set_num_threads(omp_threads);
-        printf("OpenMP mode: enabled (threads=%d) (requires build with OpenMP support)\n", omp_threads);
-    }
-    else {
-        set_use_omp(0);
-        printf("OpenMP mode: disabled\n");
-    }
-
-    if (use_mpi) {
-#if defined(USE_MPI)
-        MPI_Init(&argc, &argv);
-        MPI_Comm_rank(MPI_COMM_WORLD, &mpi_rank);
-        MPI_Comm_size(MPI_COMM_WORLD, &mpi_size);
-        set_use_mpi(1);
-        if (mpi_rank == 0) printf("MPI mode: enabled (USE_MPI), ranks=%d\n", mpi_size);
-
-        // load image only on MPI root
-        if (mpi_rank == 0) {
-            print_bmp_info(in_file);
-            if (load_bmp_grayscale(in_file, &pixels, &width, &height) != 0) {
-                printf("Failed to load %s\n", in_file);
-                MPI_Finalize();
-                return 1;
-            }
-        }
-
-        // рассылаем размеры всем процессам
-        MPI_Bcast(&width, 1, MPI_INT, 0, MPI_COMM_WORLD);
-        MPI_Bcast(&height, 1, MPI_INT, 0, MPI_COMM_WORLD);
-
-        // Если не rank 0, выделяем память под pixels
-        if (mpi_rank != 0) {
-            pixels = (unsigned char*)malloc(width * height);
-            if (!pixels) {
-                printf("Rank %d: Failed to allocate pixels\n", mpi_rank);
-                MPI_Finalize();
-                return 1;
-            }
-        }
-
-        // Рассылаем сами пиксели всем процессам
-        MPI_Bcast(pixels, width * height, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
-#else
-        printf("MPI mode requested but binary not built with USE_MPI\n");
-        return 1;
+    if (rank == 0)
+        printf("MPI ranks = %d\n", size);
 #endif
+
+    unsigned char* pixels = NULL;
+    int width = 0;
+    int height = 0;
+
+    if (rank == 0)
+    {
+        print_bmp_info("resources/input_4.bmp");
+        load_bmp_grayscale("resources/input_4.bmp", &pixels, &width, &height);
     }
-    else {
-        // Non-MPI version
-        print_bmp_info(in_file);
-        if (load_bmp_grayscale(in_file, &pixels, &width, &height) != 0) {
-            printf("Failed to load %s\n", in_file);
-            return 1;
-        }
-    }
 
-    int w = width, h = height;
-    cplx* in = (cplx*)malloc(sizeof(cplx) * w * h);
-    cplx* out = (cplx*)malloc(sizeof(cplx) * w * h);
-    cplx* rec = (cplx*)malloc(sizeof(cplx) * w * h);
-    if (!in || !out || !rec) { printf("Memory alloc failed\n"); return 1; }
+    MPI_Bcast(&width, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(&height, 1, MPI_INT, 0, MPI_COMM_WORLD);
 
-    // Преобразование в комплексный формат
-    pixels_to_cplx(pixels, w, h, in);
+    if (rank != 0)
+        pixels = malloc(width * height);
 
-    if (mpi_rank == 0) printf("\nForward DFT start\n");
+    MPI_Bcast(pixels, width * height, MPI_UNSIGNED_CHAR, 0, MPI_COMM_WORLD);
 
-    set_progress_callback(progress_cb);
+    cplx* in = malloc(sizeof(cplx) * width * height);
+    cplx* out = malloc(sizeof(cplx) * width * height);
+    cplx* rec = malloc(sizeof(cplx) * width * height);
 
-    // Замер времени для прямого DFT
+    pixels_to_cplx(pixels, width, height, in);
+
+    if (rank == 0)
+        printf("Forward DFT start\n");
+
     double t0 = MPI_Wtime();
-    dft2d(in, out, w, h);
+
+    dft2d(in, out, width, height);
+
     double t1 = MPI_Wtime();
 
-    if (mpi_rank == 0) {
-        double forward_ms = (t1 - t0) * 1000.0;
-        printf("Forward DFT is done for %.1f ms\n", forward_ms);
-    }
+    if (rank == 0)
+        printf("Forward time %.3f sec\n", t1 - t0);
 
-    // Отключить прогресс коллбек для сохранения изображений
-    set_progress_callback(NULL);
+    if (rank == 0)
+        printf("Inverse DFT start\n");
 
-    // Сохранить спектр в виде изображения (только на rank 0)
-    if (mpi_rank == 0) {
-        unsigned char* spec_img = (unsigned char*)malloc(w * h);
-        cplx_to_spectrum_image(out, w, h, spec_img);
-        if (save_bmp_grayscale(spectrum_file, spec_img, w, h) == 0) printf("Spectrum saved: %s\n", spectrum_file);
-        free(spec_img);
-    }
-
-    if (mpi_rank == 0) printf("\nInverse DFT start\n");
-
-    // Замер времени для обратного DFT
-    set_progress_callback(progress_cb);
     double t2 = MPI_Wtime();
-    idft2d(out, rec, w, h);
+
+    idft2d(out, rec, width, height);
+
     double t3 = MPI_Wtime();
 
-    if (mpi_rank == 0) {
-        double inverse_ms = (t3 - t2) * 1000.0;
-        printf("Inverse DFT is done for %.1f ms\n", inverse_ms);
-    }
+    if (rank == 0)
+        printf("Inverse time %.3f sec\n", t3 - t2);
 
-    // Сохранить восстановленное изображение (только на rank 0)
-    if (mpi_rank == 0) {
-        unsigned char* rec_img = (unsigned char*)malloc(w * h);
-        cplx_to_pixels(rec, w, h, rec_img);
-        if (save_bmp_grayscale(recon_file, rec_img, w, h) == 0) printf("Recovered image saved: %s\n", recon_file);
+    if (rank == 0)
+    {
+        unsigned char* rec_img = malloc(width * height);
+
+        cplx_to_pixels(rec, width, height, rec_img);
+
+        save_bmp_grayscale("resources/recovered.bmp",
+            rec_img,
+            width,
+            height);
+
         free(rec_img);
     }
 
-    free(pixels); free(in); free(out); free(rec);
+    free(pixels);
+    free(in);
+    free(out);
+    free(rec);
 
-    if (use_mpi) {
-#if defined(USE_MPI)
-        MPI_Finalize();
+#ifdef USE_MPI
+    MPI_Finalize();
 #endif
-    }
 
     return 0;
 }
